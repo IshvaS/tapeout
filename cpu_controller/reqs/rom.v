6a3fd329ab412a581eef7442f3707842b2689488
Daniel Gabai <dg779@cornell.edu>
2025-04-16 09:35:41 -0700

    updated rom instruction set

--- cpu_controller/reqs/rom.v
+++ cpu_controller/reqs/rom.v
@@ -5,11 +5,11 @@ module rom (
 always @(*) begin
     case (addr)
         (3'b000): out = 4'b0001;
-        (3'b001): out = 4'b1101;
-        (3'b010): out = 4'b1110;
+        (3'b001): out = 4'b1111;
+        (3'b010): out = 4'b1111;
         (3'b011): out = 4'b1111;
         (3'b100): out = 4'b1111;
-        (3'b101): out = 4'b0010;
+        (3'b101): out = 4'b1111;
         default: out = 4'b0000;
     endcase
 end
