6f97eca17fac80367f7703e8dcb6e9431f6c9967
IshvaS <ishva2k6@gmail.com>
2025-07-05 23:14:41 +0530

    Update

--- cpu_controller/rom/rom.v
+++ cpu_controller/rom/rom.v
@@ -2,15 +2,17 @@ module rom (
     input [2:0] addr,
     output reg [3:0] out
 );
+
 always @(*) begin
     case (addr)
-        (3'b000): out = 4'b0001;
-        (3'b001): out = 4'b1101;
-        (3'b010): out = 4'b1110;
-        (3'b011): out = 4'b1111;
-        (3'b100): out = 4'b1111;
-        (3'b101): out = 4'b0010;
-        default: out = 4'b0000;
+        3'b000: out = 4'b0001; // AND   mux=0 load=1
+        3'b001: out = 4'b0101; // OR    mux=0 load=1
+        3'b010: out = 4'b1001; // XOR   mux=0 load=1
+        3'b011: out = 4'b1101; // ADD   mux=0 load=1
+        3'b100: out = 4'b1101; // ADD   mux=0 load=1
+        3'b101: out = 4'b1101; // ADD   mux=0 load=1
+        default: out = 4'b0000; // NOP
     endcase
 end
+
 endmodule
