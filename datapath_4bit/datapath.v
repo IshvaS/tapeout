93b6555e326bc768640250e2f1a4bcb729785036
IshvaS <ishva2k6@gmail.com>
2025-07-05 22:47:00 +0530

    Updates

--- datapath_4bit/datapath.v
+++ datapath_4bit/datapath.v
@@ -6,31 +6,74 @@ module datapath (
     output [3:0] reg_out, alu_out
 );
 
-wire [3:0] mux_out_reg_in, reg_out_alu_in, mux_in_alu_out;
+    wire [3:0] mux_out_reg_in, reg_out_alu_in, mux_in_alu_out;
 
-mux m1 (
-    .a(mux_in_data),
-    .b(mux_in_alu_out),
-    .sel(mux_sel_data),
-    .out(mux_out_reg_in)
+    mux m1 (
+        .a(mux_in_data),
+        .b(mux_in_alu_out),
+        .sel(mux_sel_data),
+        .out(mux_out_reg_in)
+    );
+
+    register r1 (
+        .d(mux_out_reg_in),
+        .load(load),
+        .clk(clk),
+        .q(reg_out_alu_in)
+    );
+
+    alu a1 (
+        .a(reg_out_alu_in),
+        .b(alu_in_data),
+        .sel(alu_sel_data),
+        .out(mux_in_alu_out),
+        .carry_out(carry_out)
+    );
+
+    assign reg_out = reg_out_alu_in;
+    assign alu_out = mux_in_alu_out;
+
+endmodule
+
+module register (
+    input [3:0] d,
+    input clk, load,
+    output reg [3:0] q
 );
 
-register r1 (
-    .d(mux_out_reg_in),
-    .load(load),
-    .clk(clk),
-    .q(reg_out_alu_in)
+always @(posedge clk) begin
+    if (load) begin
+        q <= d;
+    end
+end
+
+endmodule
+
+module mux (
+    input [3:0] a, b,
+    input sel,
+    output [3:0] out
 );
 
-alu a1 (
-    .a(reg_out_alu_in),
-    .b(alu_in_data),
-    .sel(alu_sel_data),
-    .out(mux_in_alu_out),
-    .carry_out(carry_out)
+assign out = (sel) ? b : a;
+
+endmodule
+
+module alu (
+    input [3:0] a, b,
+    input [1:0] sel,
+    output reg [3:0] out,
+    output reg carry_out
 );
 
-assign reg_out = reg_out_alu_in;
-assign alu_out = mux_in_alu_out;
+always @(*) begin
+    case (sel)
+        2'b00: {carry_out, out} = {1'b0, a & b}; // AND
+        2'b01: {carry_out, out} = {1'b0, a | b}; // OR
+        2'b10: {carry_out, out} = {1'b0, a ^ b}; // XOR
+        2'b11: {carry_out, out} = a + b; // ADD
+        default: {carry_out, out} = 5'b0;
+    endcase
+end
 
 endmodule
--- mux_2to1/testbench.v
+++ mux_2to1/testbench.v
@@ -2,27 +2,36 @@
 
 module testbench;
 
-reg [3:0] a, b;
-reg sel;
-wire [3:0] out;
+    reg [3:0] a, b;
+    reg sel;
+    wire [3:0] out;
+    reg [1023:0] vcdfile;
 
-mux uut (
-    .a(a),
-    .b(b),
-    .sel(sel),
-    .out(out)
-);
+    mux uut (
+        .a(a),
+        .b(b),
+        .sel(sel),
+        .out(out)
+    );
 
-initial begin
+    initial begin
+        if (!$value$plusargs("vcd=%s", vcdfile)) begin
+            vcdfile = "wave.vcd";
+        end
+        $dumpfile(vcdfile);
+        $dumpvars(0,testbench);
+    end
+
+    initial begin
     //$dumpvars(0, testbench);
 
-    a = 4'b0000; b = 4'b1111; sel = 1'b0; #10;
-    $display("a=%d b = %d sel = %d out=%d", a, b, sel, out);
+        a = 4'b0000; b = 4'b1111; sel = 1'b0; #10;
+        $display("a=%d b = %d sel = %d out=%d", a, b, sel, out);
 
-    a = 4'b0000; b = 4'b1111; sel = 1'b1; #10;
-    $display("a=%d b = %d sel = %d out=%d", a, b, sel, out);
+        a = 4'b0000; b = 4'b1111; sel = 1'b1; #10;
+        $display("a=%d b = %d sel = %d out=%d", a, b, sel, out);
 
-    $finish;
-end
+        $finish;
+    end
 
 endmodule
